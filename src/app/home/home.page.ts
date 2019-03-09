ac2411147642428d5034205b3f80fb0ca3a85439
Marco Gordillo <6589245+marcopgordillo@users.noreply.github.com>
2019-03-09 12:20:36 -0500

    Added show list of expenses

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,4 +7,26 @@ import {Component} from '@angular/core';
 })
 export class HomePage {
 
+    reasonInput = '';
+    amountInput: number;
+    expensesList: {reason: string, amount: number}[] = [];
+    totalExpenses = 0;
+
+    onConfirmBtn() {
+        // console.log('It works!');
+        if (this.reasonInput.trim().length <= 0 || this.amountInput <= 0) {
+            return;
+        }
+        // console.log(this.reasonInput, this.amountInput);
+        this.expensesList.push({reason: this.reasonInput, amount: this.amountInput});
+
+        this.totalExpenses += this.amountInput;
+
+        this.clear();
+    }
+
+    clear() {
+        this.reasonInput = '';
+        this.amountInput = null;
+    }
 }
