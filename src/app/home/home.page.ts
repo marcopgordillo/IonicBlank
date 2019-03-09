8aa12001c18a8246a0103be9df348d3c5f36e054
Marco Gordillo <6589245+marcopgordillo@users.noreply.github.com>
2019-03-09 11:00:07 -0500

    Button for change a text

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+    text = 'Default starting text';
+
+    onChangeText() {
+        this.text = 'Changed!';
+    }
 }
